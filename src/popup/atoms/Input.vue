dd3e130a56411414e359d898da04bf36721b3c72
trustyoo86 <trustyoo86@gmail.com>
2020-03-23 01:59:32 +0900

    feat(CJ-2): Input renderer test
    * Input exist test

---
+++ src/popup/atoms/Input.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-input />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Input } from 'ant-design-vue';
+
+export default Vue.extend({
+  name: 'Input',
+  components: {
+    [Input.name]: Input,
+  },
+});
+</script>
