6a53404e2d5c131afce4c127a0d104f5b4175138
trustyoo86 <trustyoo86@gmail.com>
2020-03-25 01:46:13 +0900

    feat(CJ-2): Add test case

--- tests/atoms/Input.spec.ts
+++ tests/atoms/Input.spec.ts
@@ -7,4 +7,19 @@ describe('[Atoms] Input test', () => {
     const wrapper = shallowMount(Input);
     expect(wrapper.exists()).equal(true);
   });
+
+  test('Input element test', () => {
+    const wrapper = mount(Input);
+    expect(wrapper.find('input').exists()).equal(true);
+  });
+
+  test('Input props value test', () => {
+    const value = 'test';
+    const wrapper = mount(Input, {
+      propsData: {
+        value,
+      },
+    });
+    expect(wrapper.find('input').element.value).equal('test');
+  });
 });
