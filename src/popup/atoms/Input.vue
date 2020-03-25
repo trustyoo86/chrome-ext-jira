75995aadf6502288798eade783f5b2a436734885
trustyoo86 <trustyoo86@gmail.com>
2020-03-26 01:28:21 +0900

    feat(CJ-2): Add class component using typescript

--- src/popup/atoms/Input.vue
+++ src/popup/atoms/Input.vue
@@ -4,23 +4,36 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
 import { Input } from 'ant-design-vue';
 
-export default Vue.extend({
-  name: 'Input',
+@Component({
   components: {
     [Input.name]: Input,
   },
-  props: {
-    value: {
-      type: String,
-      default: '',
-    },
-  },
-  data() {
-    return {
-      val: this.value,
-    };
-  },
-});
+})
+export default class FormInput extends Vue {
+  @Prop({ default: '' })
+  public value!: string;
+
+  val: string = this.value;
+}
+// export default Vue.extend({
+//   name: 'Input',
+//   components: {
+//     [Input.name]: Input,
+//   },
+//   props: {
+//     value: {
+//       type: String,
+//       default: '',
+//     },
+//   },
+//   data() {
+//     return {
+//       val: this.value,
+//     };
+//   },
+// });
 </script>
